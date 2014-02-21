41312ed7fb21f889151516c2c82e1c2756b54c83
Andy R. Terrel <andy.terrel@gmail.com>
2014-02-21 11:00:34 -0600

    remove uses

--- blaze/compute/expr/graph.py
+++ blaze/compute/expr/graph.py
@@ -28,30 +28,17 @@ class Op(object):
     ----------
     opcode: string
         Kind of the operation, i.e. 'array' or 'kernel'
-
-    uses: [Op]
-        Consumers (or parents) of this result. This is useful to keep
-        track of, since we always start evaluation from the 'root', and we
-        may miss tracking outside uses. However, for correct behaviour, these
-        need to be retained
     """
 
     def __init__(self, opcode, dshape, *args, **metadata):
         self.opcode = opcode
         self.dshape = dshape
-        self.uses = []
         self.args = list(args)
 
         if opcode == 'kernel':
             assert 'kernel' in metadata
             assert 'overload' in metadata
         self.metadata = metadata
-
-        for arg in self.args:
-            arg.add_use(self)
-
-    def add_use(self, use):
-        self.uses.append(use)
 
     def __repr__(self):
         opcode = self.opcode
@@ -66,13 +53,10 @@ class Op(object):
         length = max(len(subtrees), len(node))
         return "%s\n%s" % (node.center(len(subtrees) / 2), subtrees.center(length))
 
-# ______________________________________________________________________
-# Graph constructors
-
-ArrayOp    = partial(Op, array)
+ArrayOp = partial(Op, array)
 
 # Kernel application. Associated metadata:
 #   kernel: the blaze.function.Kernel that was applied
 #   overload: the blaze.overload.Overload that selected for the input args
 
-KernelOp   = partial(Op, kernel)
+KernelOp = partial(Op, kernel)
